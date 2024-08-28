3109ad36b25395a99e106310c8789de3d9e5fdac
KwonMyungWoo <redkwon1119@gamil.com>
2024-08-28 17:50:53 +0900

    24.08.28 세번째 완료.

--- homework3/scripts.js
+++ homework3/scripts.js
@@ -1,14 +1,12 @@
-const $messege = document.querySelector('messege');
-const $search = document.querySelector('.search');
+const SearchMessege = document.querySelector('.search-messege');
+const SearchButton = document.querySelector('.naver-search');
 
 const search = () => {
-  const keyword = $messege.value;
-  location.href = `https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ssc=tab.nx.all&query=${keyword}`;
+  const SearchKey = SearchMessege.value;
+  location.href = `https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ssc=tab.nx.all&query=${SearchKey}`;
 };
 
-$messege.addEventListener('keyup', (e) => {
-  if (e.key == 'Enter') {
-    search();
-  }
+SearchMessege.addEventListener('keyup', (e) => {
+  if (e.key == 'Enter') search();
 });
-$search.addEventListener('click', search);
+SearchButton.addEventListener('click', search);
